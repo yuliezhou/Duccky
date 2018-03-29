39eb6e43aee87b99eca4d0789f9b2e4e24d63e1f
yuliezhou <798651174@qq.com>
2018-03-29 23:27:13 +0800

    yk

--- pc/yq/js/index.js
+++ pc/yq/js/index.js
@@ -1,49 +1,38 @@
-
-$("#header li a").click(function(){
-    return true;
-})
-
-$(window).scroll(function() {
-        var scrollH = $(window).scrollTop();
-        if (scrollH > 0) {
-            $("#header").addClass('back_header');
-            $('.son-menu.dropdown-menu a').addClass('white_a');
-            $('.son-menu.dropdown-menu').addClass('men_back');
-        } else {
-            $("#header").removeClass('back_header');
-            $('.son-menu.dropdown-menu').removeClass('men_back');
-            $('.son-menu.dropdown-menu a').removeClass('white_a');
+    //回到顶部
+    $('#top').on('click',function() {
+         $('body,html').animate({'scrollTop':0},1000)
+    });
+    //顶部导航栏
+    var headerH = $("#header").offset().top;
+    $(window).scroll(function(e) {
+        p = $(this).scrollTop();
+        if (p >headerH ) { //下滚  
+            $('.empty').show();
+            $('.logo_inner').css({
+                display: 'inline-block'
+            });
+            $('#header').addClass('pofixed');
+        } else { //上滚  
+            $('.empty').hide();
+            $('.logo_inner').css({
+                display: 'none'
+            });         
+            $('#header').removeClass('pofixed');
         }
-        // 判断滚动的距离。
-        // banner 的高度
-       var bannerH = $('.banner1').height();
-       if(scrollH >= bannerH/2){
-           // 动画效果-公司简介
-           $('.tran_box3').addClass('animated fadeInUp');
-           $('.tran_box4').addClass('animated fadeInRight');
-            // 动画效果-加入我们
-            $('.join_tran1').addClass('animated1 fadeInDown');       
-            $('.join_tran2').addClass('animated2 fadeInDown');       
-            $('.join_tran3').addClass('animated3 fadeInDown');       
-            $('.join_tran4').addClass('animated4 fadeInDown');   
-            $('.ditu').addClass('animated4 fadeInDown');   
-       }
-        
+    });
+    //提交表单验证
+    $('#submit').on('click',function(){
+        var zz1 = $('#zz1').val();
+        var zz2 = $('#zz2').val();
+        var name = $('#name').val();
+        var phone = $('#phone').val();
+        if(zz1 == ''){
+            layer.msg('请输入您的资质类型');
+        }else if(zz2 == ''){
+            layer.msg('请输入您的资质等级');
+        }else if(name == ''){
+            layer.msg('请输入您的姓名');
+        }else if(phone == ''){
+            layer.msg('请输入您的电话号码');
+        }
     })
-//分享
-window._bd_share_config = {
-    "common": {
-        "bdSnsKey": {},
-        "bdText": "",
-        "bdMini": "2",
-        "bdMiniList": false,
-        "bdPic": "",
-        "bdStyle": "2",
-        "bdSize": "24"
-    },
-    "share": {}
-};
-with(document) 0[(getElementsByTagName('head')[0] || body).appendChild(createElement('script')).src = 'http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion=' + ~ ( - new Date() / 36e5)];
-// 首页-end------------------------------
-
- 
