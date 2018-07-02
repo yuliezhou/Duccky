2e6938afc5c3dd58388db618779b0bc4a9d76fbb
silkwood <798651174@qq.com>
2018-07-02 18:41:38 +0800

    demo

---
+++ stu/迪卡侬/js/index.js
@@ -0,0 +1,54 @@
+    function showTime() {
+        nowtime = new Date();
+        year = nowtime.getFullYear();
+        month = nowtime.getMonth() + 1;
+        date = nowtime.getDate();
+        document.getElementById("timer").innerText = year + "年" + month + "月" + date + " " + nowtime.toLocaleTimeString();
+    }
+    setInterval("showTime()", 1000);
+    var n = 0;
+    var images = document.getElementsByClassName('banimg');
+    images[0].style.display = 'block';
+    var timer = setInterval(function() {
+        images[n].style.display = 'none';
+        n = ++n == 3 ? 0 : n;
+        images[n].style.display = 'block';
+    }, 2000);
+    // var login = document.getElementById('login');
+    // var input_box = document.getElementById('input_box');
+    // login.onclick= function(){
+    //     input_box.style.display = 'block';
+    // }
+    // var close = document.getElementById('close');
+    // var ad = document.getElementById('ad');
+    // close.onclick= function(){
+    //     ad.style.display = 'none';
+    // }
+    // var close1 = document.getElementById('close1');
+    // close1.onclick= function(){
+    //     input_box.style.display = 'none';
+    // }
+    var leftbtn = document.getElementById('leftbtn');
+    var rightbtn = document.getElementById('rightbtn');
+    leftbtn.onclick= function(){
+        for(var j=0;j<images.length;j++){
+            images[j].style.display = 'none';
+        }
+        clearInterval(timer)
+        n--;
+        if(n<0){
+            n=2
+        }
+        images[n].style.display = 'block';
+    }
+    rightbtn.onclick= function(){
+        for(var j=0;j<images.length;j++){
+            images[j].style.display = 'none';
+        }
+        clearInterval(timer)
+        n++;
+        if(n>2){
+            n=0
+        }
+        images[n].style.display = 'block';
+    }
