6d931abf61996038b2c41c3bd1474ed83427c25e
silkwood <798651174@qq.com>
2018-09-03 18:43:17 +0800

    备份

--- 小程序/90days/pages/mycourse/mycourse.js
+++ 小程序/90days/pages/mycourse/mycourse.js
@@ -2,9 +2,86 @@ const app = getApp()
 var api = require('../../utils/api.js')
 Page({
     data: {
-
+		courseIdx:1,
+		modalShow:false,
+		courseList1:[
+			{
+				date:'8月16日',
+				week:'周一',
+				timer:'14:40',
+				courseTitle:'高效燃脂11',
+				course_p:'塑形·舞蹈·体态纠正1',
+				coursePrice:'18:00-19:00 ¥69',
+				viewBool:true
+			},
+			{
+				date:'8月17日',
+				week:'周二',
+				timer:'14:50',
+				courseTitle:'高效燃脂12',
+				course_p:'塑形·舞蹈·体态纠正2',
+				coursePrice:'18:00-19:00 ¥69',
+				viewBool:false
+			},
+		],
+		courseList2:[
+			{
+				date:'8月16日',
+				week:'周一',
+				timer:'14:40',
+				courseTitle:'高效燃脂21',
+				course_p:'塑形·舞蹈·体态纠正1',
+				coursePrice:'18:00-19:00 ¥69',
+				viewBool:true
+			},
+			{
+				date:'8月17日',
+				week:'周二',
+				timer:'14:50',
+				courseTitle:'高效燃脂22',
+				course_p:'塑形·舞蹈·体态纠正2',
+				coursePrice:'18:00-19:00 ¥69',
+				viewBool:false
+			},
+		],
+		courseList3:[
+			{
+				date:'8月16日',
+				week:'周一',
+				timer:'14:40',
+				courseTitle:'高效燃脂31',
+				course_p:'塑形·舞蹈·体态纠正1',
+				coursePrice:'18:00-19:00 ¥69',
+				viewBool:true
+			},
+			{
+				date:'8月17日',
+				week:'周二',
+				timer:'14:50',
+				courseTitle:'高效燃脂32',
+				course_p:'塑形·舞蹈·体态纠正2',
+				coursePrice:'18:00-19:00 ¥69',
+				viewBool:false
+			},
+		],
     },
     onLoad: function(res) {
 
+    },
+    chooseCourse:function (res) {
+    	var id = res.currentTarget.dataset.id;
+    	this.setData({
+    		courseIdx:id
+    	})
+    },
+    viewQR:function () {
+    	this.setData({
+    		modalShow:true
+    	})
+    },
+    closeModal:function(){
+    	this.setData({
+    		modalShow:false
+    	})    	
     }
 })
