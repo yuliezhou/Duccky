218b4ca11fc04bfbbc9ad7a4dd8e473883cf6604
silkwood <798651174@qq.com>
2018-09-04 17:51:33 +0800

    90mia update

--- 小程序/90days/pages/store/store.js
+++ 小程序/90days/pages/store/store.js
@@ -2,7 +2,30 @@ const app = getApp()
 var api = require('../../utils/api.js')
 Page({
     data: {
-
+    	imgUrls:['../../images/swiper.png','../../images/swiper.png','../../images/swiper.png'],
+    	areaList:['重庆','成都'],
+    	storeList:[
+			{
+				imgurl:'../../images/swiper.png',
+				name:'重庆光电园1',
+				address:'重庆市渝北区龙山街道冉家坝 爱奇艺9楼'
+			},
+			{
+				imgurl:'../../images/swiper.png',
+				name:'重庆光电园2',
+				address:'重庆市渝北区龙山街道冉家坝 爱奇艺9楼重庆市渝北区龙山街道冉家坝 爱奇艺9楼'
+			},
+			{
+				imgurl:'../../images/swiper.png',
+				name:'重庆光电园3',
+				address:'重庆市渝北区龙山街道冉家坝 爱奇艺9楼'
+			},
+			{
+				imgurl:'../../images/swiper.png',
+				name:'重庆光电园4',
+				address:'重庆市渝北区龙山街道冉家坝 爱奇艺9楼'
+			},
+    	]
     },
     onLoad: function(res) {
 
