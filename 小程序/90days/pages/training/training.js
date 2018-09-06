684b07740eea7210c69678cd557df9035f07c2c1
silkwood <798651174@qq.com>
2018-09-06 14:57:41 +0800

    company 90Mia update

--- 小程序/90days/pages/training/training.js
+++ 小程序/90days/pages/training/training.js
@@ -2,7 +2,32 @@ const app = getApp()
 var api = require('../../utils/api.js')
 Page({
     data: {
-
+		trainingList:[
+			{
+				time:'0816 19:00',
+				name:'燃烧体脂燃烧体脂',
+				cal:200,
+				heart:120
+			},
+			{
+				time:'0817 19:00',
+				name:'燃烧体脂',
+				cal:300,
+				heart:120
+			},
+			{
+				time:'0818 19:00',
+				name:'燃烧体脂',
+				cal:500,
+				heart:120
+			},
+			{
+				time:'0818 19:00',
+				name:'燃烧体脂',
+				cal:600,
+				heart:120
+			},
+		]
     },
     onLoad: function(res) {
 
