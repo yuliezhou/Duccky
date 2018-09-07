181b0a62b39e1e572011a55ec80884cefe218439
silkwood <798651174@qq.com>
2018-09-07 11:53:45 +0800

    company 90Mia update

--- 小程序/90days/pages/pay/pay.js
+++ 小程序/90days/pages/pay/pay.js
@@ -2,7 +2,8 @@ const app = getApp()
 var api = require('../../utils/api.js')
 Page({
     data: {
-		coursetype:'single'
+		coursetype:''
+		// coursetype:'single'
     },
     onLoad: function(res) {
 		// 通过路径传递参数 -->我约定为coursetype-->可以自己约定(用于判断页面显示为单次课程还是其他,默认可以不传)
